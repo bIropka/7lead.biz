1ae4fa67d988e503a5d2097ce8c7f47905bf30d5
bIropka <biropka@gmail.com>
2016-10-26 13:00:04 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,1 +1,37 @@
+$(document).ready(function () {
+
+    /******************************
+     ******* init scripts *********
+     ******************************/
+
+    /*setTimeout(function() {
+        $('.sli').animate({opacity: 1});
+    }, 500);
+
+    /******************************
+     ******* other scripts ********
+     ******************************/
+
+    /*$('.burger').click(function() {
+        $(this).siblings('.nav').slideToggle();
+    });*/
+
+    $('.tab-controls li').click(function() {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    /*******************************
+     ******* slider scripts ********
+     ******************************/
+
+    $('.slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
+});
+
 
