0764d2768144c904ee6bf20f59e8f9dcee2467ed
bIropka <biropka@gmail.com>
2016-10-26 16:53:55 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,21 +4,26 @@ $(document).ready(function () {
      ******* init scripts *********
      ******************************/
 
-    /*setTimeout(function() {
-        $('.sli').animate({opacity: 1});
+    setTimeout(function() {
+        $('.slider').animate({opacity: 1});
     }, 500);
 
     /******************************
      ******* other scripts ********
      ******************************/
 
-    /*$('.burger').click(function() {
-        $(this).siblings('.nav').slideToggle();
-    });*/
+    $('.burger').click(function() {
+        $(this).siblings('nav ul').slideToggle();
+    });
 
     $('.tab-controls li').click(function() {
-        $(this).siblings('.active').removeClass('active');
-        $(this).addClass('active');
+        if(!$(this).hasClass('active')) {
+            var nextIndex = $(this).index();
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            $('.slider.active').removeClass('active');
+            $('.slider').eq(nextIndex).addClass('active');
+        }
     });
 
 
@@ -27,9 +32,24 @@ $(document).ready(function () {
      ******************************/
 
     $('.slider').slick({
-        arrows: false,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1181,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
 });
